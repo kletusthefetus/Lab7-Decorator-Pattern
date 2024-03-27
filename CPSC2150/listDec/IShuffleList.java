d653a5bb3e8a6ad54291f2827a557f35da68de64
James Maner <jwmaner@g.clemson.edu>
2024-03-27 10:25:13 -0400

    Added general files

---
+++ CPSC2150/listDec/IShuffleList.java
@@ -0,0 +1,18 @@
+import package CPSC2150.listDec
+
+
+
+public interface IShuffleList extends List
+{
+    default void shuffle(int swaps)
+    {
+
+    }
+
+    default void swap(int i, int j)
+    {
+
+    }
+
+
+}
